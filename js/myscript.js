712497d97c196d8f55fc7b49a00bc23b66fc3722
Mrs Omotosho <busayomiomotosho10@gmail.com>
2015-10-23 22:50:34 -0700

    the first version

---
+++ js/myscript.js
@@ -0,0 +1,16 @@
+/*$(document).ready(function(){
+	
+	$("#wishes_text"). hide();
+
+	$("#cardflip").click(function(){
+		$("#welcome_text").fadeOut("fast");
+		$("#wishes_text"). css({'-webkit-transform': 'rotate(360deg)', 'transform':'rotate(360deg)'}) .fadeIn("slow");
+		
+		$(".birthday_card"). css({
+			'-webkit-transform' : 'rotate(360deg)',
+			'transform' : 'rotate(360deg)'
+		});
+
+	});
+
+})*/
